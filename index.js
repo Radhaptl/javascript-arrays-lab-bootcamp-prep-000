8353c365ee7374d580539cd79f42b66f12268281
Radha Patel <radhapatel99@gmail.com>
2017-12-18 02:37:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const app = "I don't do much."
 const kittens = ['Milo', 'Otis', 'Garfield'];
 
- /*describe('destructivelyAppendKitten(name)', function() {
-    it('appends a kitten to the end of the kittens array', function() {
-      destructivelyAppendKitten('Ralph')
-      expect(window.kittens).toEqual(["Milo", "Otis", "Garfield", "Ralph"])*/
-      
 function destructivelyAppendKitten(name){
 kittens.push(name)
 return kittens
