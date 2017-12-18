0ef3205329ce604ccc64a0ecd0ad612483c3dfaf
Radha Patel <radhapatel99@gmail.com>
2017-12-18 02:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ kittens.push(name)
 return kittens
 }
 
-/*
-  describe('destructivelyPrependKitten(name)', function() {
-    it('prepends a kitten to the beginning of the kittens array', function() {
-      destructivelyPrependKitten("Bob")
-
-      expect(window.kittens).toEqual(["Bob", "Milo", "Otis", "Garfield"])
-    })
-  })*/
-  
 function destructivelyPrependKitten(name) { 
   kittens.unshift(name)
   return kittens;
